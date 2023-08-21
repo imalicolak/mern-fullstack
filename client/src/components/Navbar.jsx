a3f1ed2a95cbee727884e172ee79aeb773350021
imalicolak <19acolak99@gmail.com>
2023-08-21 15:03:29 -0500

    Created user model

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
-		<div className='bg-cyan-200 lg:w-[40%] md:w-[50%] sm:w-[70%] xs:w-[90%] xs:rounded-none rounded-3xl mx-auto drop-shadow-xl mt-4 xs:mt-0  '>
-			<div className='flex justify-between items-center  max-w-2xl mx-auto p-4 '>
+		<div className='bg-cyan-200 w-[90%] lg:w-[85%] md:w-[80%] sm:w-[75%] rounded-full mx-auto  mt-4  '>
+			<div className='flex justify-evenly items-center gap-2 lg:p-4 md:p-4 sm:p-4 max-w-2xl mx-auto p-4 sticky '>
 				<Link to='/'>
 					<h1 className=' '>
-						MERN{' '}
+						MERN
 						<block className='font-bold'> AUTH </block>
 					</h1>
 				</Link>
-				<ul className='flex gap-4'>
+				<ul className='flex gap-5 items-center justify-center'>
 					<Link to='home'>
 						<li>Home</li>
 					</Link>
@@ -19,10 +19,14 @@ const Navbar = () => {
 						<li>About</li>
 					</Link>
 					<Link to='sign-in'>
-						<li>Sign-in</li>
+						<li className='bg-blue-300 p-3 rounded-full'>
+							Sign-in
+						</li>
 					</Link>
 					<Link to='sign-up'>
-						<li>Sign-up</li>
+						<li className='bg-blue-300 p-3 rounded-full'>
+							Sign-up
+						</li>
 					</Link>
 				</ul>
 			</div>
